4e03b3c2a3884093c7e6c57b6f227f679b894ddf
Aaron Antrim <aaron@trilliumtransit.com>
2016-02-25 16:14:30 -0800

    this account looks reasonable

--- Separate rows for each stop in a pattern.sql
+++ Separate rows for each stop in a pattern.sql
@@ -1,12 +1,16 @@
+WITH result_set AS(
 WITH timed_patterns AS (
-SELECT row_number() over() as timed_pattern_id,MIN(trips.trip_id) as one_trip,string_agg( trips.trip_id::text, ', ' ORDER BY sequences.min_arrival_time ) AS trips_list, sequences.stops_pattern, stop_time_intervals,trips.agency_id,routes.route_id,routes.route_short_name,routes.route_long_name,directions.direction_label,headsigns.headsign,time_intervals_result.min_arrival_time,time_intervals_result.min_departure_time
+
+WITH timed_patterns_sub AS ( 
+
+SELECT MIN(trips.trip_id) as one_trip,string_agg( trips.trip_id::text, ', ' ORDER BY sequences.min_arrival_time ) AS trips_list, sequences.stops_pattern, stop_time_intervals,trips.agency_id,routes.route_id,routes.route_short_name,routes.route_long_name,directions.direction_label,headsigns.headsign,time_intervals_result.min_arrival_time,time_intervals_result.min_departure_time
 FROM trips
 INNER JOIN (
 
 	 SELECT  string_agg(stop_times.stop_id::text , ', ' ORDER BY stop_times.stop_sequence ASC) AS stops_pattern, stop_times.trip_id, MIN( stop_times.arrival_time ) AS min_arrival_time
 
 	 FROM stop_times
-	 WHERE stop_times.agency_id IN (1,3,267,392,392)
+	 WHERE stop_times.agency_id IN (392)
 	 GROUP BY stop_times.trip_id
 	 ) AS sequences ON trips.trip_id = sequences.trip_id
 
@@ -20,10 +24,10 @@ INNER JOIN
 		 INNER JOIN (
 		 SELECT MIN( arrival_time ) AS min_arrival_time, MIN( departure_time ) AS min_departure_time,  trip_id
 		 FROM stop_times
-		 WHERE agency_id IN (1,3,267,392)
+		 WHERE agency_id IN (392)
 		 GROUP BY stop_times.trip_id
 		 ) min_trip_times ON stop_times.trip_id = min_trip_times.trip_id
-	 WHERE stop_times.agency_id in (1,3,267,392)
+	 WHERE stop_times.agency_id in (392)
 	 GROUP BY min_trip_times.trip_id,min_arrival_time,min_departure_time
 	) AS time_intervals_result
 
@@ -33,8 +37,10 @@ LEFT JOIN routes on trips.route_id = routes.route_id
 LEFT JOIN directions ON trips.direction_id = directions.direction_id
 LEFT JOIN headsigns on trips.headsign_id = headsigns.headsign_id
 
-WHERE trips.agency_id IN (1,3,267,392)
-GROUP BY stops_pattern,stop_time_intervals,trips.agency_id,routes.route_id,routes.route_short_name,routes.route_long_name,directions.direction_label,headsigns.headsign,time_intervals_result.min_arrival_time, time_intervals_result.min_departure_time),
+WHERE trips.agency_id IN (392)
+GROUP BY stops_pattern,stop_time_intervals,trips.agency_id,routes.route_id,routes.route_short_name,routes.route_long_name,directions.direction_label,headsigns.headsign,time_intervals_result.min_arrival_time, time_intervals_result.min_departure_time
+
+) select row_number() over() as timed_pattern_id, * from timed_patterns_sub ),
 
 stop_patterns AS (
 
@@ -44,18 +50,20 @@ SELECT DISTINCT string_agg(stop_times.stop_id::text , ', ' ORDER BY stop_times.s
 
 	 FROM stop_times
 	 inner join trips on stop_times.trip_id = trips.trip_id
-	 WHERE trips.agency_id IN (1,3,267,392,392) AND trips.based_on IS NULL
+	 WHERE trips.agency_id IN (392,392) AND trips.based_on IS NULL
 	 GROUP BY stop_times.trip_id)
 SELECT unique_patterns.stops_pattern,row_number() over() as pattern_id from unique_patterns
 
 )
 
-SELECT timed_patterns.agency_id,stop_times.stop_id, 
+SELECT timed_patterns.agency_id,stop_times.stop_id, timed_pattern_id::text || ' - ' || pattern_id::text AS concat_pattern,
 dense_rank() over (partition by timed_pattern_id order by stop_times.stop_sequence) as stop_order,
-timed_pattern_id, stop_patterns.pattern_id,
+timed_pattern_id,
+stop_patterns.pattern_id,
 CASE WHEN stop_times.arrival_time IS NOT NULL THEN (stop_times.arrival_time - min_arrival_time)::text END as arrival_time,
 CASE WHEN stop_times.departure_time IS NOT NULL THEN (stop_times.departure_time - min_departure_time)::text END as departure_time,
-one_trip,trips_list,timed_patterns.stops_pattern,stop_time_intervals,route_id,route_short_name,route_long_name,direction_label,headsign FROM timed_patterns
+one_trip,trips_list,stop_patterns.stops_pattern,stop_time_intervals,route_id,route_short_name,route_long_name,direction_label,headsign FROM timed_patterns
 LEFT JOIN stop_times ON timed_patterns.one_trip = stop_times.trip_id
-LEFT JOIN stop_patterns ON timed_patterns.stops_pattern = stop_patterns.stops_pattern
+inner JOIN stop_patterns ON timed_patterns.stops_pattern = stop_patterns.stops_pattern
 ORDER BY pattern_id,timed_pattern_id ASC, stop_times.stop_sequence ASC
+) SELECT COUNT(DISTINCT timed_pattern_id),COUNT(DISTINCT concat_pattern),COUNT(DISTINCT pattern_id),COUNT(DISTINCT stops_pattern) FROM result_set
