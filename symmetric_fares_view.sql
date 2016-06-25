b51a90d2b323da0d1c2834d7a28dfbc3eaa32733
ed-g <edward.groth@gmail.com>
2016-06-24 18:42:04 -0700

    now works with route_id and contains_id

--- symmetric_fares_view.sql
+++ symmetric_fares_view.sql
@@ -1,27 +1,32 @@
+/* -- ED: this is not needed any longer, I've updated the symmetric view to
+   -- work with route_id and contains_id. 2016-06-24
 CREATE VIEW views.play_migrate_fare_rules_no_route_no_contains AS
 SELECT *
 FROM play_migrate_fare_rules fr
 WHERE fr.route_id IS NULL AND fr.contains_id IS NULL;
 
 ALTER VIEW views.play_migrate_fare_rules_no_route_no_contains OWNER TO trillium_gtfs_group;
+*/
 
-CREATE VIEW views.play_migrate_fare_rules_symmetric AS
 -- SELECT DISTINCT required for the time being since there appear to be some
 -- duplicate fare rules. ED 2016-06-23
-SELECT DISTINCT fr1.fare_id, fr1.origin_id AS zone_id_a, fr1.destination_id AS zone_id_b
--- FROM play_migrate_fare_rules fr1
--- JOIN play_migrate_fare_rules fr2
-FROM views.play_migrate_fare_rules_no_route_no_contains fr1
-JOIN views.play_migrate_fare_rules_no_route_no_contains fr2
+CREATE OR REPLACE VIEW views.play_migrate_fare_rules_symmetric AS
+SELECT DISTINCT fr1.fare_id
+     , fr1.origin_id AS zone_id_a
+     , fr1.destination_id AS zone_id_b
+     , fr1.route_id 
+     , fr1.contains_id
+FROM play_migrate_fare_rules fr1
+JOIN play_migrate_fare_rules fr2
     ON fr1.fare_id = fr2.fare_id
     AND fr1.origin_id = fr2.destination_id
     AND fr1.destination_id = fr2.origin_id
+    AND (fr1.route_id = fr2.route_id 
+         OR (fr1.route_id IS NULL AND fr2.route_id IS NULL))
+    AND (fr1.contains_id = fr2.contains_id 
+         OR (fr1.contains_id IS NULL AND fr2.contains_id IS NULL))
 WHERE fr1.origin_id < fr1.destination_id     
-      AND fr2.origin_id > fr2.destination_id ;
-      -- AND fr1.route_id IS NULL 
-      -- AND fr2.route_id IS NULL 
-      -- AND fr1.contains_id IS NULL 
-      -- AND fr1.contains_id IS NULL
+      AND fr2.origin_id > fr2.destination_id;
 
 ALTER VIEW views.play_migrate_fare_rules_symmetric OWNER TO trillium_gtfs_group;
 
@@ -33,35 +38,62 @@ BEGIN
         -- Assert that there are no fare rules applying to fare_id which either
         -- go from zone_id_a to zone_id_b or from zone_id_b to zone_id_a.
         -- NOTE that zone_id_a must always be strictly less than zone_id_b
-        RAISE NOTICE 'deleted from play_migrate_fare_rules_symmetric fare_id % zone_id_a % zone_id_b %'
-            , OLD.fare_id
-            , OLD.zone_id_a
-            , OLD.zone_id_b;
+        RAISE NOTICE 'deleted from play_migrate_fare_rules_symmetric:';
+        RAISE NOTICE 'fare_id % route_id % zone_id_a % zone_id_b % contains_id %'
+                    , OLD.fare_id
+                    , OLD.route_id
+                    , OLD.zone_id_a
+                    , OLD.zone_id_b
+                    , OLD.contains_id;
 
-        DELETE FROM views.play_migrate_fare_rules_no_route_no_contains
+            -- FIXME: add logic to check for & propegate route_id, and
+            -- contains_id if they exist.
+            
+            -- UPDATE for route_id, or contains_id should update the underlying tables.
+            -- UPDATE for origin_id or destination_id should delete the old and
+            -- create new rows in underlying tables.
+
+        DELETE FROM play_migrate_fare_rules
             WHERE fare_id = OLD.fare_id 
               AND (   (origin_id = OLD.zone_id_a AND destination_id = OLD.zone_id_b)
-                   OR (origin_id = OLD.zone_id_b AND destination_id = OLD.zone_id_a));
-        
+                   OR (origin_id = OLD.zone_id_b AND destination_id = OLD.zone_id_a))
+              AND (route_id = OLD.route_id 
+                   OR (route_id IS NULL AND OLD.route_id IS NULL))
+              AND  (contains_id = OLD.contains_id 
+                   OR (contains_id IS NULL AND OLD.contains_id IS NULL)) ; 
     END IF;
     IF TG_OP IN ('INSERT') THEN 
         -- Assert that there are a pair of fare rules applying to fare_id
         -- going from zone_id_a to zone_id_b, and from zone_id_b to zone_id_a.
-        RAISE NOTICE 'inserted into play_migrate_fare_rules_symmetric fare_id % zone_id_a % zone_id_b %'
-            , NEW.fare_id
-            , NEW.zone_id_a
-            , NEW.zone_id_b;
+        RAISE NOTICE 'inserted into play_migrate_fare_rules_symmetric:';
+        RAISE NOTICE ' fare_id % route_id % zone_id_a % zone_id_b % contains_id %'
+                    , NEW.fare_id
+                    , NEW.route_id
+                    , NEW.zone_id_a
+                    , NEW.zone_id_b
+                    , NEW.contains_id;
+
         -- DELETE followed by INSERT acts as a poor man's UPSERT.
         -- This way we avoid creating duplicate rules.
         -- In an ideal world there would be a table constraint preventing duplicates.
-        DELETE FROM views.play_migrate_fare_rules_no_route_no_contains
+
+        DELETE FROM play_migrate_fare_rules
             WHERE fare_id = NEW.fare_id 
               AND (   (origin_id = NEW.zone_id_a AND destination_id = NEW.zone_id_b)
-                   OR (origin_id = NEW.zone_id_b AND destination_id = NEW.zone_id_a));
-        INSERT INTO views.play_migrate_fare_rules_no_route_no_contains
-            (fare_id, origin_id, destination_id)
-        VALUES (NEW.fare_id, NEW.zone_id_a, NEW.zone_id_b)
-             , (NEW.fare_id, NEW.zone_id_b, NEW.zone_id_a);
+                   OR (origin_id = NEW.zone_id_b AND destination_id = NEW.zone_id_a))
+              AND (route_id = NEW.route_id 
+                   OR (route_id IS NULL AND NEW.route_id IS NULL))
+              AND  (contains_id = NEW.contains_id 
+                   OR (contains_id IS NULL AND NEW.contains_id IS NULL)) ; 
+
+        --DELETE FROM views.play_migrate_fare_rules_no_route_no_contains
+        --    WHERE fare_id = NEW.fare_id 
+        --      AND (   (origin_id = NEW.zone_id_a AND destination_id = NEW.zone_id_b)
+        --           OR (origin_id = NEW.zone_id_b AND destination_id = NEW.zone_id_a));
+        INSERT INTO play_migrate_fare_rules
+            (fare_id, route_id, origin_id, destination_id, contains_id)
+        VALUES (NEW.fare_id, NEW.route_id, NEW.zone_id_a, NEW.zone_id_b, NEW.contains_id)
+             , (NEW.fare_id, NEW.route_id, NEW.zone_id_b, NEW.zone_id_a, NEW.contains_id);
     END IF;
 
     IF TG_OP IN ('INSERT', 'UPDATE') THEN 
@@ -87,29 +119,18 @@ INSTEAD OF DELETE OR INSERT ON views.play_migrate_fare_rules_symmetric
 FOR EACH ROW EXECUTE PROCEDURE views.play_migrate_fare_rules_symmetric_trigger();
 
 
-    -- FIXME. The terminology and table organization for various categories of
-    -- fare_rules could use rationalization and improvement.
-    -- 
-    -- I've called this view play_migrate_fare_rules_not_symmetric instead of
-    -- play_migrate_fare_rules_asymmetric, since it is quite possible that 
-    -- fare_rules involving NOT NULL values for route_id and contains_id may be
-    -- symmetric by some definition, they simply don't meet our criterea for
-    -- "symmetric" routes.
-    --
-    -- Long term, I suspect we'll want to make other classifications for
-    -- fare_rules depending on if route_id and contains_id are NULL, with their
-    -- own distinct tables and/or views.
-CREATE VIEW views.play_migrate_fare_rules_not_symmetric AS
+CREATE OR REPLACE VIEW views.play_migrate_fare_rules_asymmetric AS
 SELECT *
 FROM play_migrate_fare_rules fr
-WHERE    (    fr.route_id    IS NULL
-          AND fr.contains_id IS NULL
-          AND (fr.fare_id, least(fr.origin_id, fr.destination_id), greatest(fr.origin_id, fr.destination_id))
-              NOT IN (SELECT fare_id, zone_id_a, zone_id_b from views.play_migrate_fare_rules_symmetric))
-      OR fr.route_id    IS NOT NULL 
-      OR fr.contains_id IS NOT NULL;
+WHERE    ((fr.fare_id
+         , fr.route_id
+         ,    least(fr.origin_id, fr.destination_id) -- zone a
+         , greatest(fr.origin_id, fr.destination_id) -- zone b
+         , fr.origin_id )
+              NOT IN (SELECT fare_id, route_id, zone_id_a, zone_id_b, origin_id 
+                      FROM views.play_migrate_fare_rules_symmetric));
 
-ALTER VIEW views.play_migrate_fare_rules_not_symmetric OWNER TO trillium_gtfs_group;
+ALTER VIEW views.play_migrate_fare_rules_asymmetric OWNER TO trillium_gtfs_group;
 
 -- It would be good to verify that: 
 --
