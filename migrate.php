4fb09728047ecdc2777719fc1530c0bf219e2e47
ed-g <edward.groth@gmail.com>
2016-06-26 17:52:08 -0700

    show query

--- migrate.php
+++ migrate.php
@@ -619,6 +619,9 @@ $migrate_fare_rules_query = "
         ; ";
 $result = db_query($migrate_fare_rules_query);
 
+echo '\n <br/ >fare rules query';
+echo '\n <br/ >\n' .  $migrate_fare_rules_query;
+
 $get_least_unused_fare_rule_id = "
     SELECT 1 + MAX(fare_rule_id)
     FROM {$table_prefix}_fare_rules";
