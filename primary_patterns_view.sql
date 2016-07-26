b8738fa865a362ceee95e29ff5dae81bdfd1d09a
ed-g <edward.groth@gmail.com>
2016-07-26 13:03:09 -0700

    count multiple trips, where headway_secs is used

--- primary_patterns_view.sql
+++ primary_patterns_view.sql
@@ -1,30 +1,67 @@
-create view views.play_migrate_route_primary_patterns 
+create or replace view views.migrate_route_primary_patterns 
 as 
 select route_id, direction_id, primary_pattern_id from
-( select
+( 
+  select
     route_id, direction_id, pattern_id,
-    count(*), first_value(pattern_id) over w as primary_pattern_id
-  from play_migrate_timed_patterns
-  join play_migrate_patterns using (pattern_id)
+    sum(
+        CASE WHEN (headway_secs IS NULL or end_time IS NULL) 
+            THEN 1
+        ELSE floor(1 + extract(epoch from end_time-start_time) / headway_secs)
+        END
+        ) AS num_trips,
+    first_value(pattern_id) over w as primary_pattern_id
+  from migrate_timed_patterns
+  join migrate_patterns using (pattern_id)
+  join migrate_schedules using (timed_pattern_id)
   group by route_id, direction_id, pattern_id
   window w as (partition by route_id, direction_id
-               order by route_id, direction_id, count(*) desc, pattern_id)
-  order by route_id, direction_id, count(*) desc, pattern_id) most_common
-group by route_id, direction_id, primary_pattern_id order by route_id, direction_id;
+               order by route_id, direction_id, 
+                        sum(
+                            CASE WHEN (headway_secs IS NULL or end_time IS NULL) 
+                                THEN 1
+                                ELSE floor(1 + extract(epoch from end_time-start_time) / headway_secs)
+                                END
+                            )   desc,
+                        pattern_id)
+  order by route_id, direction_id, num_trips desc, pattern_id 
 
+) most_common
+group by route_id, direction_id, primary_pattern_id order by route_id, direction_id;
 
 ALTER view views.play_migrate_route_primary_patterns owner to trillium_gtfs_group;
 
-create view views.migrate_route_primary_patterns 
+
+create or replace view views.migrate_route_primary_patterns 
 as 
-select route_id, primary_pattern_id from
-( select route_id, pattern_id, count(*), first_value(pattern_id) over w as primary_pattern_id
+select route_id, direction_id, primary_pattern_id from
+( 
+  select
+    route_id, direction_id, pattern_id,
+    sum(
+        CASE WHEN (headway_secs IS NULL or end_time IS NULL) 
+            THEN 1
+        ELSE floor(1 + extract(epoch from end_time-start_time) / headway_secs)
+        END
+        ) AS num_trips,
+    first_value(pattern_id) over w as primary_pattern_id
   from migrate_timed_patterns
   join migrate_patterns using (pattern_id)
-  group by route_id, pattern_id
-  window w as (partition by route_id order by route_id, count(*) desc, pattern_id)
-  order by route_id, count(*) desc, pattern_id) most_common
-group by route_id, primary_pattern_id order by route_id;
+  join migrate_schedules using (timed_pattern_id)
+  group by route_id, direction_id, pattern_id
+  window w as (partition by route_id, direction_id
+               order by route_id, direction_id, 
+                        sum(
+                            CASE WHEN (headway_secs IS NULL or end_time IS NULL) 
+                                THEN 1
+                                ELSE floor(1 + extract(epoch from end_time-start_time) / headway_secs)
+                                END
+                            )   desc,
+                        pattern_id)
+  order by route_id, direction_id, num_trips desc, pattern_id 
+
+) most_common
+group by route_id, direction_id, primary_pattern_id order by route_id, direction_id;
 
 ALTER view views.migrate_route_primary_patterns owner to trillium_gtfs_group;
 
