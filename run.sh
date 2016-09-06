76e9951b71aa99b072b198f0ebce534c4b90835c
ed-g <edward.groth@gmail.com>
2016-09-06 13:01:04 -0700

    fix typo

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export SRC_SCHMEA=public; export DST_SCHEMA=play_migrate; (php migrate.php ; php migrate_addendum.php) | tee tmp/$DST_SCHEMA.log
+export SRC_SCHEMA=public; export DST_SCHEMA=play_migrate; (php migrate.php ; php migrate_addendum.php) | tee tmp/$DST_SCHEMA.log
